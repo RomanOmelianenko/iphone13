ce84c2c896791217ccbede950648c6531136570b
RomanOmelianenko <r_omelyanenko@ukr.net>
2022-02-19 12:40:00 +0200

    tabs

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,34 +1,42 @@
-const links = document.querySelectorAll('.header-menu__item a');
+const scrollFunc = () => {
+  const links = document.querySelectorAll('.header-menu__item a');
+  const linkCharacteristics = document.querySelector('.card-details__link-characteristics');
 
-// Используум пакет seamless-scroll-polyfill для кроссбраузерности (теперь во всех браузерах будет работать плавный скрол)
-seamless.polyfill();
+  const newArray = [...links, linkCharacteristics];
+  // console.log('####: newArray', newArray);
 
-links.forEach((element, _index, _array) => {
-  
-  element.addEventListener('click', (event) => {
-    event.preventDefault();
+  // Используум пакет seamless-scroll-polyfill для кроссбраузерности (теперь во всех браузерах будет работать плавный скрол)
+  seamless.polyfill();
 
-    // Плавный скрол до нужной секции
-    const id = element.getAttribute('href').substring(1);
+  newArray.forEach((element, _index, _array) => {
     
-    const section = document.getElementById(id);
+    element.addEventListener('click', (event) => {
+      event.preventDefault();
 
-    if (section) {
-      // section.scrollIntoView({
-      //   behavior: 'smooth',
-      //   block: 'start'
-      // })
-      seamless.scrollIntoView(section, {
-        behavior: 'smooth',
-        block: 'start'
-      })
+      // Плавный скрол до нужной секции
+      const id = element.getAttribute('href').substring(1);
       
-    } else {
-      seamless.scrollIntoView(document.querySelector("#characteristics"), {
-        behavior: "smooth",
-        block: "center",
-        inline: "center",
-      });
-    };
+      const section = document.getElementById(id);
+
+      if (section) {
+        // section.scrollIntoView({
+        //   behavior: 'smooth',
+        //   block: 'start'
+        // })
+        seamless.scrollIntoView(section, {
+          behavior: 'smooth',
+          block: 'start'
+        })
+        
+      } else {
+        seamless.scrollIntoView(document.querySelector("#characteristics"), {
+          behavior: "smooth",
+          block: "center",
+          inline: "center",
+        });
+      };
+    });
   });
-});
+};
+
+scrollFunc();
---
+++ scripts/tabs.js
@@ -0,0 +1,63 @@
+const tabsFunc = () => {
+  const tabs = document.querySelectorAll(".card-detail__change");
+  const tabsTitle = document.querySelector(".card-details__title");
+  const tabsPrice = document.querySelector(".card-details__price");
+  const tabsImage = document.querySelector(".card__image_item");
+  const tabsMemory = document.querySelector(".description__memory");
+
+  const tabsOptions = [
+    {
+      name: "Graphite",
+      memory: "128",
+      price: "60000",
+      image: "img/iPhone-graphite.webp",
+    },
+    {
+      name: "Silver",
+      memory: "256",
+      price: "65000",
+      image: "img/iPhone-silver.webp",
+    },
+    {
+      name: "Sierra Blue",
+      memory: "512",
+      price: "70000",
+      image: "img/iPhone-sierra_blue.webp",
+    },
+  ];
+
+  const changeContent = (index) => {
+    // console.log('####: index', index);
+    // console.log(tabsOptions[index].name);
+    tabsTitle.textContent = `Смартфон Apple iPhone 13 Pro ${tabsOptions[index].memory}GB ${tabsOptions[index].name}`;
+
+    tabsPrice.textContent = `${tabsOptions[index].price}₽`;
+
+    tabsImage.setAttribute("src", tabsOptions[index].image);
+
+    tabsMemory.textContent = `Встроенная память (ROM) ${tabsOptions[index].memory} ГБ`;
+  };
+
+  const changeActiveTabs = (indexClickedTab) => {
+    // console.log('####: indexClickedTab', indexClickedTab);
+    tabs.forEach((tab, index) => {
+      tab.classList.remove("active");
+
+      if (index === indexClickedTab) {
+        tab.classList.add("active");
+      }
+    });
+
+    changeContent(indexClickedTab);
+  };
+
+  tabs.forEach((tab, index) => {
+    tab.addEventListener("click", () => {
+      changeActiveTabs(index);
+    });
+  });
+
+  changeContent(0);
+};
+
+tabsFunc();
